ed76d10dc9fe1cef5864a47238c11a8c85419087
Samuel Oche Adejo <143835433+Samueladejo@users.noreply.github.com>
2023-12-21 12:07:59 +0100

    Add files via upload
    new commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+btn.addEventListener("click", clickFunc);
+function clickFunc(){
+	window.location.href="http://www.facebook.com";
+}
+
+var newEl = document.getElementById("CTA1");
+
+/*
+var slides =  document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".button");
+
+let currentSlide = 1;
+
+//for image
+/*var manualNav = function(manual){
+	slides(manual).classList.add("active");
+	btns(manual).classList.add("active");
+}
+
+btns.forEach((button, i) => {
+	button.addEventListener("click", () => {
+		manualNav(i);
+		currentSlide = 1
+	})
+});*/
